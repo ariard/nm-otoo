5e9b52cf96ede49060475b8bba368f4aa83801e8
Antoine Riard <ariard@student.42.fr>
2018-02-06 18:22:31 +0100

    [repo] clean trash

--- objs/parse_sections.d
+++
@@ -1,8 +0,0 @@
-objs/parse_sections.o: src/parse_sections.c includes/nm.h \
-  includes/../libft/includes/libft.h libft/includes/ft_types.h \
-  libft/includes/error.h libft/includes/libft.h libft/includes/color.h \
-  libft/includes/cliopts.h libft/includes/rs.h libft/includes/lst.h \
-  libft/includes/dlst.h libft/includes/btree.h libft/includes/hashtab.h \
-  libft/includes/str.h libft/includes/sstr.h libft/includes/math.h \
-  libft/includes/mytime.h libft/includes/get_next_line.h \
-  libft/includes/net.h libft/includes/sys.h includes/elf.h
--- test.c
+++
@@ -1,8 +0,0 @@
-#include <mach-o/loader.h>
-#include <stdio.h>
-
-int	main(void)
-{
-	struct mach_header	hello;
-	printf("size mach-o header : [%d]", sizeof(hello));
-}
